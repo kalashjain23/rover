f4eae8c309a1ec34aa044c179278cfea82209c3d
Kalash <kalashjain513@gmail.com>
2023-05-30 20:31:38 +0530

    Using ros2_control to control the rover

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -29,9 +29,23 @@ def generate_launch_description():
         output='screen'
     )
     
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
     
